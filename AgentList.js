f0381327d15cb386c9a4d6a4975263a20d854fc7
piyush9245 <71199693+piyush9245@users.noreply.github.com>
2020-11-01 11:59:30 +0530

    Add files via upload

---
+++ AgentList.js
@@ -0,0 +1,27 @@
+var Pincode1 = [
+    {
+        ShopName: "Kartik Elecctricals",
+        Address: "Mekahara Road,Moudhapara",
+        ContactNo:9988776655
+    },
+    {
+        ShopName: "Bharat Sales",
+        Address: "Shastri Bazar Mandir,Sadar Bazar",
+        ContactNo:9911991199
+    },
+    {   
+        ShopName: "Lalit Service Center",
+        Address: "Behind DKS Hospital,Ghadi Chowk",
+        ContactNo:9876543219
+    },
+    {
+        ShopName: "Sundrani Repair World",
+        Address: "Samvet Shikhar Building,Near Rajbandha Maidan",
+        ContactNo:0771492001
+    },
+    {
+        ShopName: "Mangal Services",
+        Address: "Beside Laxmi Medical Hall,Kutchery Chowk",
+        ContactNo:8899889988
+    },
+]
