31b064607eafe71830c18da6f67a50b6e1139c39
piyush9245 <71199693+piyush9245@users.noreply.github.com>
2020-11-06 16:11:10 +0530

    Add files via upload

---
+++ AgentList.js
@@ -0,0 +1,220 @@
+var Pincode2 = [
+    {
+        ShopName: "Kartik Elecctricals",
+        Address: "Mekahara Road,Moudhapara",
+        ContactNo:9988776655
+    },
+    {
+        ShopName: "Bharat Sales",
+        Address: "Shastri Bazar Mandir,Sadar Bazar",
+        ContactNo:9911991199
+    },
+    {   
+        ShopName: "Lalit Service Center",
+        Address: "Behind DKS Hospital,Ghadi Chowk",
+        ContactNo:9876543219
+    },
+    {
+        ShopName: "Sundrani Repair World",
+        Address: "Samvet Shikhar Building,Near Rajbandha Maidan",
+        ContactNo:7714920011
+    },
+    {
+        ShopName: "Mangal Services",
+        Address: "Beside Laxmi Medical Hall,Kutchery Chowk",
+        ContactNo:8899889988
+    },
+]
+
+
+var Pincode3 = [
+    {
+        ShopName: "Mahavir Elecctricals",
+        Address: "Urla Industrial Complex,Birgaon",
+        ContactNo:9987654321
+    },
+    {
+        ShopName: "Nishad Repairing Stores",
+        Address: "Urla Basti,Birgaon",
+        ContactNo:9999977777
+    },
+    {   
+        ShopName: "Gurukripa Udyog",
+        Address: "Transport Nagar,Urla",
+        ContactNo:8899889988
+    },
+    {
+        ShopName: "Singh Service Point",
+        Address: "Jagriti Nagar,Urkura",
+        ContactNo:9977889988
+    },
+    {
+        ShopName: "Rameshwar Repairing House",
+        Address: "Budhwari Bazar,Tilak Nagar",
+        ContactNo:6655449988
+    },
+]
+
+
+var Pincode4 = [
+    {
+        ShopName: "R S Marketing",
+        Address: "Sector-5,Devendra Nagar",
+        ContactNo:7755889966
+    },
+    {
+        ShopName: "Chouhan Electricals",
+        Address: "Indra Aawas Colony,New Timber Market,Fhafhadih",
+        ContactNo:9169916991
+    },
+    {   
+        ShopName: "Shri Gurudev Repairing",
+        Address: "Paras Nagar Chowk,Devendra Nagar",
+        ContactNo:9876543219
+    },
+    {
+        ShopName: "K M Services",
+        Address: "Patel Ground,Fhafhadih",
+        ContactNo:8855996699
+    },
+    {
+        ShopName: "Ahuja Repairs",
+        Address: "Avanti Bai Chowk",
+        ContactNo:9998887779
+    },
+]
+
+
+var Pincode5 = [
+    {
+        ShopName: "N S Service Point",
+        Address: "Vidhan Sabha Road",
+        ContactNo:9898989898
+    },
+    {
+        ShopName: "Lokesh Hardware & Repairs",
+        Address: "Raipur Road,Beside DPS School",
+        ContactNo:7778889999
+    },
+    {   
+        ShopName: "Rahul Electricals",
+        Address: "Mandhar Railway Station Road",
+        ContactNo:9873216549
+    },
+]
+
+
+var Pincode6 = [
+    {
+        ShopName: "Sagar Repairing Point",
+        Address: "New Purena,Amlidih",
+        ContactNo:9638527419
+    },
+    {
+        ShopName: "Sirish Shop",
+        Address: "A-4,Mahaveer Nagar",
+        ContactNo:9999997777
+    },
+    {   
+        ShopName: "Bhushan TV Center",
+        Address: "Parshuram Nagar,Infront of Udyog Bhawan",
+        ContactNo:8888899999
+    },
+    {
+        ShopName: "Welcome Services",
+        Address: "Basant Vihar,Mahaveer Nagar",
+        ContactNo:8978978979
+    },
+    {
+        ShopName: "SSD General Repairs",
+        Address: "Near Pujari Fuels,New Rajendra Nagar",
+        ContactNo:9966339966
+    },
+]
+
+
+var Pincode7 = [
+    {
+        ShopName: "Vinayak Repairing House",
+        Address: "Gali No-2,Vidhan Sabha Road",
+        ContactNo:7897897897
+    },
+    {
+        ShopName: "Punam Electricals",
+        Address: "Kachna Road,Near Railway Crossing",
+        ContactNo:8956895689
+    },
+    {   
+        ShopName: "Verma Electricals",
+        Address: "Mowa Main Road,Raipur",
+        ContactNo:7899877899
+    },
+    {
+        ShopName: "Anurag Repair Service",
+        Address: "New Shanti Nagar,Govind Nagar",
+        ContactNo:9638527410
+    },
+    {
+        ShopName: "Dhanwantari Auto Repairs",
+        Address: "Shankar Nagar Road,Geetanjali Colony",
+        ContactNo:9999991111
+    },
+]
+
+
+var Pincode8 = [
+    {
+        ShopName: "Jyoti Home Repairs",
+        Address: "Sector-4,WRS Colony",
+        ContactNo:9696966969
+    },
+    {
+        ShopName: "Rahul Retail & Repair Shop",
+        Address: "Mahalaxmi Market No.108,Bilaspur Road",
+        ContactNo:9895978989
+    },
+    {   
+        ShopName: "Manav Motors",
+        Address: "Khamtarai Main Road,Saptgiri Colony",
+        ContactNo:7897897898
+    },
+    {
+        ShopName: "Yadav Gas & General Repairing",
+        Address: "39/27-1,Bhanpuri,Raipur",
+        ContactNo:9494949494
+    },
+    {
+        ShopName: "Trimurti Home Solution",
+        Address: "Near Jhanda Chowk,Gudhiyari",
+        ContactNo:9191919119
+    },
+]
+
+
+var Pincode9 = [
+    {
+        ShopName: "Rakesh Elecctricals",
+        Address: "Nemichand Gali,Tel Ghani Naka",
+        ContactNo:9292929292
+    },
+    {
+        ShopName: "Raghav Agency",
+        Address: "Near Gulmohar Park,Kota Main Road",
+        ContactNo:9393939393
+    },
+    {   
+        ShopName: "Khilesh Repairing",
+        Address: "Beside Jayanti Bhawan,Karma Chowk",
+        ContactNo:7719999888
+    },
+    {
+        ShopName: "Mahamaya Traders",
+        Address: "Gogaon Main Road",
+        ContactNo:9497989596
+    },
+    {
+        ShopName: "Krishna Care",
+        Address: "Behind Durga Petrol Pump,Gondwara",
+        ContactNo:9291939596
+    },
+]
